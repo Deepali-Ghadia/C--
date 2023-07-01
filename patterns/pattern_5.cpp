7dcd34c957bb869bc45d90a176caa40ea3ba4545
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-07-01 15:57:59 +0530

    Printing triangular pattern using while loop

---
+++ patterns/pattern_5.cpp
@@ -0,0 +1,34 @@
+/*
+Print the pattern
+*
+* *
+* * *
+* * * *
+*/
+
+
+#include<iostream>
+using namespace std;
+
+ int main(){
+
+    int n, i = 1;
+
+    cout << "Enter number of rows" << endl;
+    cin >> n;
+
+    while (i <= n){
+        int j = 1;
+
+        while (j <=i){
+            cout << "*" << " ";
+            j++;
+        }
+
+        cout << endl;
+        i++;
+    }
+    return 0;
+
+ }
+
