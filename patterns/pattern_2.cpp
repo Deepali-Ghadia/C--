ce092f607def42028209ccf2f71fd595a1072c51
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-07-01 14:59:34 +0530

    Printing pattern using nested While loops

--- patterns/pattern_2.cpp
+++ patterns/pattern_2.cpp
@@ -10,19 +10,34 @@ using namespace std;
 
 int main(){
 
-    int n=4, i, j;
+    int n, i, j;
 
     // take number of lines as input from user
     cout << "Enter value of n: \n";
     cin >> n;
 
     // using for loop
+    cout << "Using Nested For Loops: " << endl;
     for (i=1; i<=n; i++){
         for(j=1; j<=n; j++){
             cout << j << " "; 
         }
         cout << endl;
     }
+    cout << endl;
 
+    // using while loop
+    cout << "Using Nested While Loops: " << endl;
+    i = 1;
+    while (i <= n){
+        j = 1;
+
+        while (j <= n){
+            cout << j << " ";
+            j += 1;
+        }
+        cout << endl;
+        i += 1;
+    }
     return 0;
 }
